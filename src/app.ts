c1cf97a3936cd7b58b692ee799dcf434001dcc5c
turpin_l <turpin_l@etna-alternance.net>
2024-10-24 19:39:03 +0200

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from '@fastify/cors';
 import fastifySwagger from '@fastify/swagger';
 import fastifySwaggerUI from '@fastify/swagger-ui';
 import { authenticationContract } from '@zcorp/wheelz-contracts';
@@ -20,6 +21,9 @@ export const app = Fastify({
 
 app.setErrorHandler((error, _, reply) => {
   reply.status(error.statusCode ?? 500).send({ message: error.message, data: error.cause });
+});
+app.register(cors, {
+  origin: '*',
 });
 
 server.registerRouter(authenticationContract.authentication, authRouter, app, {
